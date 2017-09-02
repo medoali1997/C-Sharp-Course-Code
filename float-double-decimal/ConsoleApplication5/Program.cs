63176904692029797243ee5908fd30b4d1aa81fa
Mohamed Ali <mohamedalifarouk@hotmail.com>
2017-09-02 06:08:01 +0200

    Float Double Decimal
    Different between Float Double and Decimal

---
+++ float-double-decimal/ConsoleApplication5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float flVar = 1F / 3;
+            double dlVar = 1F / 3;
+            decimal dcVar = 1 / 33;
+
+            Console.WriteLine("float value ={0} \ndouble value = {1}  \ndecimal value = {2}", flVar, dlVar, dcVar);
+
+            Console.ReadKey();
+        }
+    }
+}
